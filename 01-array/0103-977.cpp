2356853cf3027de2219fb3816d4f7bfa2cc5f58c
un01s <96022876+un01s@users.noreply.github.com>
2023-10-18 20:49:34 -0600

    update

---
+++ 01-array/0103-977.cpp
@@ -0,0 +1,24 @@
+/*
+ * leetcode 977
+ *
+ * double-pointer: left and right
+ */
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        int k = n-1;
+        vector<int> res(n, 0);
+        for (int i = 0, j = n-1; i <= j; ) {
+            if (nums[i]*nums[i] < nums[j]*nums[j]) {
+                res[k--] = nums[j]*nums[j];
+                j--;
+            } else {
+                res[k--] = nums[i]*nums[i];
+                i++;
+            }
+        }
+        return res;
+    }
+};
