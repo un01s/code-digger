d613e35ecc61a2fe3418a4eebe20bb98bcbb6942
un01s <96022876+un01s@users.noreply.github.com>
2023-10-25 09:52:52 -0600

    update-linkedlist

--- 02-linkedlist/0201-203.cpp
+++ 02-linkedlist/0201-203.cpp
@@ -35,9 +35,30 @@ public:
 
         head = dummyHead->next;
         delete dummyHead;
+
         return head;
     }
 };
+
+// recursive
+class Solution2 {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        // the list can be viewed as a head node plus one shorter list
+        // 1. handle if the head is null
+        if (head == nullptr) return head;
+        // 2. handle the shorter list without the head
+        ListNode* res =  removeElements(head->next, val);
+        // 3. handle the head node if not null
+        if (head->val == val) 
+            return res;
+        else {
+            head->next =res;
+            return head;
+        }    
+    }
+};
+
 
 int main() {
 
