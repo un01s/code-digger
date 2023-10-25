36b6fd75a5439e18481b9f3386b9a78fd8a1188c
un01s <96022876+un01s@users.noreply.github.com>
2023-10-25 09:31:21 -0600

    update-linkedlist

---
+++ 02-linkedlist/0201-203.cpp
@@ -0,0 +1,45 @@
+/*
+ * leetcode 203
+ * remove element from the linked list
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        
+        ListNode* cur = dummyHead;
+        while (cur->next != NULL) {
+            if(cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            } else {
+                cur = cur->next;
+            }
+        }
+
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;
+    }
+};
+
+int main() {
+
+    return 0;
+}
